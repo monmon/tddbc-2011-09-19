0eacd467761696917fee13a066bfaff0f9005d3c
monmon <lesamoureuses@gmail.com>
2013-11-07 02:17:36 +0900

    NGワードを複数追加できるように配列で保持するように変更

--- src/WordFilter.class.php
+++ src/WordFilter.class.php
@@ -3,16 +3,20 @@ class WordFilter
 {
     const REPLACE_STRING = '<censored>';
 
-    protected $_ngWord;
+    protected $_ngWords = array();
 
     public function __construct($ngWord)
     {
-        $this->_ngWord = $ngWord;
+        $this->_ngWords[] = $ngWord;
     }
 
     public function detect($text)
     {
-        if (strpos($text, $this->_ngWord) === false) {
+        if (empty($this->_ngWords)) {
+            return false;
+        }
+
+        if (strpos($text, $this->_ngWords[0]) === false) {
             return false;
         }
 
@@ -21,6 +25,10 @@ class WordFilter
 
     public function censor($text)
     {
-        return str_replace($this->_ngWord, self::REPLACE_STRING, $text);
+        if (empty($this->_ngWords)) {
+            return $text;
+        }
+
+        return str_replace($this->_ngWords[0], self::REPLACE_STRING, $text);
     }
 }
